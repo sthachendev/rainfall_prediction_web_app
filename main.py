4523f4cc79da52ae00fdab1f3581d48da7542a37
sthachendev <sthachendev@gmail.com>
2022-11-28 21:46:18 +0600

    update

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pickle
 import numpy as np
 from streamlit_option_menu import option_menu
-from explore_page import show_data_frame, show_heat_map, rfa
+from explore_page import show_data_frame, rfa
 from PIL import Image
+
+
 def show_predict_page():
 
-    # with st.sidebar:
-    
     selected = option_menu(
             menu_title = '',
             options=['Home','Predict','Explore','Team'],
@@ -26,20 +26,14 @@ def show_predict_page():
     if selected == 'Team':
         team()
 
+
 def home():
     st.write("###### Accuracy of rainfall forecasting has great importance for countries like Bhutan whose economy is largely dependent on hydro-power project and agriculture. ")
-    # col1, col2 = st.columns(2)
 
     st.title('Prediction Accuracy')
     st.write('The accuracy of the our model is 67 on train dataset and it is 71 on test dataset.')
     st.write()
-    # with col1:
-        # image = Image.open('./Image/cloud-rain.png')
-        # st.image(image)
-    
-    # st.write('Rainfall')
-    # show_rainfall_chart()
-    # show_rainfall_chart_group()
+
     rfa()
 
 def predict_page():
@@ -69,22 +63,20 @@ def predict_page():
     with col2:
         month_input = st.selectbox('Month', month_array)
         minT = st.number_input('Min Temperature')
-        ws = st.number_input('WS')
+        ws = st.number_input('WS (Wind Speed)')
         
     for i in location_array:
         if location_input == i:
             location_data = location_array_encoded[location_array.index(i)]
-            #st.write(location_data)
 
     for i in month_array:
         if month_input == i:
             month_data = month_array.index(i)+1
-            #st.write(month_data)
 
     with col1:
         year = st.selectbox('Year',year_array)
         maxT = st.number_input('Max Temperature (c)')
-        rh = st.number_input('RH')
+        rh = st.number_input('RH (Relative Humidity)')
 
     predict = st.button('Predict')
 
@@ -98,19 +90,41 @@ def predict_page():
         if (result>0):
             st.write('Rainfall', result[0].round(2),'ml')
         else:
-            st.write('No rainfall', result[0].round(2))
+            st.write('No rainfall')
     
 
 def explore():
     st.title('Data Exploration & Data Visualization')
     exp = st.expander('Dataset')
     with exp:
+        st.write('The dataset is collected from the National Center of Meteorology and Hydrology. The dataset contain features such as location, year, month, date, maximum temperature, minimum temperature, relative humidity, wind speed, and rainfall. The data was recorded from 2020 to 2021 from 10 different locations. The meteorology station records the values of the environmental variable every day for each year directly from the devices in the station.')
         show_data_frame()
 
     exp = st.expander('Heatmap')
     with exp:
-        show_heat_map()
+        image = Image.open('./Image/heatmap.png')
+        st.image(image, 'Heatmap')
     
+    exp = st.expander('Rainfall vs Max Temperature')
+    with exp:
+        image = Image.open('./Image/f1.png')
+        st.image(image, 'Rainfall vs Max Temperature')
+    
+    exp = st.expander('Rainfall vs Min Temperature')
+    with exp:
+        image = Image.open('./Image/f2.png')
+        st.image(image, 'Rainfall vs Min Temperature')
+    
+    exp = st.expander('Rainfall vs Relative Humidity')
+    with exp:
+        image = Image.open('./Image/f3.png')
+        st.image(image, 'Rainfall vs Relative Humidity')
+    
+    exp = st.expander('Rainfall vs Wind Speed')
+    with exp:
+        image = Image.open('./Image/f4.png')
+        st.image(image, 'Rainfall vs Wind Speed')
+
 
 def team():
     st.title('Team Members')
@@ -132,13 +146,3 @@ def team():
     with col4:
         image = Image.open('./Image/user.jpeg')
         st.image(image, 'Sonam Tobden')
-
-    # expander = st.expander('asdad')
-
-    # with expander:
-    #     st.write("""
-    #     The chart above shows some numbers I picked for you.
-    #     I rolled actual dice for these, so they're *guaranteed* to
-    #     be random.
-    #     """)
-
